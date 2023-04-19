77512ebb3474d917856215b39d97fecbfac376f6
Amo Jlt <98520349+Amoryne@users.noreply.github.com>
2023-04-19 14:07:52 +0200

    Auto-generated commit [ci skip]

--- src/app/store/catalogue.actions.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "./types/product";
-
-export namespace Catalogue{
-    export class AddItem {
-        static readonly type = '[Catalogue] AddItem';
-        constructor(public item: Product){}
-    }
-}
